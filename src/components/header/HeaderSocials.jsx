bb222a514de3ad1782e29b714b4f4e31c387ea78
Hamidevity <127018991+Hamidevity@users.noreply.github.com>
2023-06-30 09:32:45 -0700

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,7 +9,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
+        <a href="https://github.com/Hamidevity/Modern-portfolio" target="_blank"><FaGithub/></a>
         <a href="https://dribbble.com" target="_blank"><FiDribbble/></a>
     </div>
   )
